44ab96231aeb020f861f6340b6ac408b1bb4184d
Terrence Bell <terrencekbell@gmail.com>
2020-03-11 14:36:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
- def initialize(name, breed)
+ def initialize(name, breed="Mutt")
    @name = name 
    @breed = breed 
  end 
  def name=(name)
    @name = name 
  end 
- def breed=(breed= "Mutt")
- 
+ def breed=(breed)
+ @breed = breed 
  end 
  end 
   
